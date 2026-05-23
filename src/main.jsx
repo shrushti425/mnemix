99fa424755cd42210ab0043cadf578765d1b5176
shrushti425 <smraut3434@gmail.com>
2026-05-23 19:32:53 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+function AppRouter({ children }) {
+  const useHashRouter = typeof window !== 'undefined' && window.location.protocol === 'file:';
+  const Router = useHashRouter ? HashRouter : BrowserRouter;
+
+  return <Router>{children}</Router>;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <AppRouter>
       <App />
-    </BrowserRouter>
+    </AppRouter>
   </React.StrictMode>
 );
